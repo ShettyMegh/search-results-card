6481cb544c81966603f7b626cbc706bb46b3d5c8
Meghanath Shetty <meghanathshetty108@gmail.com>
2025-09-17 19:07:04 +0530

    added event listener for char "s" to make input focused

--- src/feature/search-card/components/index.tsx
+++ src/feature/search-card/components/index.tsx
@@ -1,15 +1,36 @@
 import "../css/search-card.css";
 import SearchRow from "./search-row";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 const SearchCard = () => {
   const [searchValue, setSearchValue] = useState("");
+  const ref = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     console.log({ searchValue });
   }, [searchValue]);
+
+  useEffect(() => {
+    const handleKeydown = (e: KeyboardEvent) => {
+      console.log(e);
+      if (
+        (e.key === "s" || e.key === "S") &&
+        document.activeElement !== ref.current
+      ) {
+        e.stopPropagation();
+        e.preventDefault();
+        ref.current?.focus();
+      }
+    };
+    addEventListener("keydown", handleKeydown);
+
+    return () => {
+      removeEventListener("keydown", handleKeydown);
+    };
+  }, []);
   return (
     <div className="card">
       <SearchRow
+        ref={ref}
         value={searchValue}
         onChange={(e) => {
           setSearchValue(e.target.value);
